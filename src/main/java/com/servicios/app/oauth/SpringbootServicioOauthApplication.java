94409e4bf91082e88a2f75736859130fdba0771d
Adrian1426 <adrianhh1426@gmail.com>
2021-02-01 11:57:20 -0600

    configuración inicial de nuestro servicio de autorización

--- src/main/java/com/servicios/app/oauth/SpringbootServicioOauthApplication.java
+++ src/main/java/com/servicios/app/oauth/SpringbootServicioOauthApplication.java
@@ -2,8 +2,10 @@ package com.servicios.app.oauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient //habilitar que sea cliente de eureka, es opcional, automaticamente con la dependecia se habilita
 public class SpringbootServicioOauthApplication {
 
 	public static void main(String[] args) {
