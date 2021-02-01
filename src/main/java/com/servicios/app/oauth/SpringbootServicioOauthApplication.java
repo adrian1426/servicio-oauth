994dc56928d1ef9ad61e58b7c40f7a643ef8ba22
Adrian1426 <adrianhh1426@gmail.com>
2021-02-01 15:38:17 -0600

    probando con postman

--- src/main/java/com/servicios/app/oauth/SpringbootServicioOauthApplication.java
+++ src/main/java/com/servicios/app/oauth/SpringbootServicioOauthApplication.java
@@ -1,17 +1,33 @@
 package com.servicios.app.oauth;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient //habilitar que sea cliente de eureka, es opcional, automaticamente con la dependecia se habilita
 @EnableFeignClients
-public class SpringbootServicioOauthApplication {
+public class SpringbootServicioOauthApplication implements CommandLineRunner{
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootServicioOauthApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		String password="front1234";
+		
+		for (int i = 0; i < 4; i++) {
+			String pwdBcrypt=passwordEncoder.encode(password);
+			System.out.println(pwdBcrypt);
+		}
 	}
 
 }
